095ad160f582976bd3794bc25b239bdbb4f29e62
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2024-11-03 12:15:07 +0200

    Added semi colons on all console.log statements, and called the getComputerChoice function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Hello,World");
 
-function getComputerChoice(choice){
+function getComputerChoice(){
     let choice1 = "rock";
     let choice2 = "paper";
     let choice3 = "scissors";
@@ -8,17 +8,19 @@ function getComputerChoice(choice){
     let selectChoice = prompt("Chose one of the option Rock, Paper, Scissor: ");
     
     if(selectChoice === choice1){
-        console.log(choice1)
+        console.log(choice1);
     }
     if (selectChoice === choice2){
-        console.log(choice2)
+        console.log(choice2);
     }
     if(selectChoice === choice3){
-        console.log(choice3)
+        console.log(choice3);
     }
     else{
-        console.log("Choose a valid option")
+        console.log("Choose a valid option");
     }
     
-    return choice;
+    return selectChoice;
 }
+
+console.log(getComputerChoice());
