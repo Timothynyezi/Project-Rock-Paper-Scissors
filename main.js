d9180108fe716b9fabfac0a4f2b08a0cc3d21ea4
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2024-11-03 11:14:33 +0200

    Created a getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("Hello,World");
+
+function getComputerChoice(choice){
+    let choice1 = "rock";
+    let choice2 = "paper";
+    let choice3 = "scissors";
+
+    let selectChoice = prompt("Chose one of the option Rock, Paper, Scissor: ");
+    
+    return choice;
+}
