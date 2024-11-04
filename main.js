883e93236d756e6824ca87060111b924a0b47fdb
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2024-11-04 10:08:48 +0200

    Deleted redundant lines of code in the getHumanChoice function

--- main.js
+++ main.js
@@ -16,17 +16,9 @@ function getHumanChoice(){
     let selectChoice = prompt("Chose one of the option Rock, Paper, Scissor: ");
     
     if(selectChoice === choice1){
-        console.log(choice1);
+        
     }
-    if (selectChoice === choice2){
-        console.log(choice2);
-    }
-    if(selectChoice === choice3){
-        console.log(choice3);
-    }
-    //else{
-        //console.log("Choose a valid option");
-    //}
+    
     
     return selectChoice;
 }
